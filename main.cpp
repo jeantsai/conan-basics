eb6978e4b0dd0953324716e8f3d5326ced418fda
jeantsai <jeantsai@qq.com>
2023-04-16 15:28:51 +0800

    Prepare cliapp for mygrep exercise

--- main.cpp
+++ main.cpp
@@ -7,26 +7,29 @@ using namespace clipp;
   Demo: commands may not be optional, thus a global "-v" cannot co-exists with any command
 */
 int main(int argc, char* argv[]) {
-    enum class mode { find, help, version };
+    enum class mode { find, version, help };
     mode selected = mode::help;
+    std::string pattern = "";
+    std::string glob = "";
 
-    auto cli = ((
-            command("find").set(selected, mode::find) |
-            command("help").set(selected, mode::help)
-        ).optional(),
-        option("-v", "--version").set(selected, mode::version).doc("show version")
+    auto cli = (
+        (
+            opt_value("pattern", pattern).set(selected, mode::find) % "searching pattern"
+            & opt_value("path") % "searching path"
+        ),
+        option("-v", "--version").set(selected, mode::version) % "show version",
+        option("-h", "--help").set(selected, mode::help) % "show help information"
     );
 
-    if ( parse(argc, argv, cli) ) {
-        switch( selected ) {
-            case mode::find: std::cout << "find action" << std::endl; break;
-            case mode::help: std::cout << make_man_page(cli, argv[0]) << std::endl; break;
-            case mode::version: std::cout << "Version 0.1.0\n\n"; break;
-        }
-
-    } else {
+    if ( !parse(argc, argv, cli) ) {
         std::cout << make_man_page(cli, argv[0]) << std::endl;
+        exit(1);
     }
 
-    std::cout << "Hello, world!\n";
+    switch( selected ) {
+        case mode::find: std::cout << "find action" << std::endl; break;
+        case mode::help: std::cout << make_man_page(cli, "mygrep") << std::endl; break;
+        case mode::version: std::cout << "Version 0.1.0\n\n"; break;
+    }
+
 }
